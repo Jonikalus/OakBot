f9fa8a93840aa03c0ba248644617fdda57b63a97
Jeanolos <iaf41nuerrenber@gso-koeln.de>
2016-01-12 21:54:29 +0100

    Added SQLite functions for later implementation

--- OakBot/Utils.cs
+++ OakBot/Utils.cs
@@ -4,16 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Data.SQLite;
+using System.IO;
 
 namespace OakBot
 {
     class Utils
     {
+        //Extracts the OAuth token from an URL
         public static string getAuthTokenFromUrl(string absoluteUrl)
         {
             Match url = Regex.Match(absoluteUrl, "access_token=(?<token>[a-zA-Z0-9]+)&");
             string token = url.Groups["token"].Value.Trim();
             return token;
+        }
+
+        //Creates a database by file name
+        public static void CreateDatabase(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                SQLiteConnection.CreateFile(fileName);
+            }
+        }
+
+        //Connects to database by file name
+        public static SQLiteConnection ConnectDatabase(string databaseName)
+        {
+            return new SQLiteConnection(string.Format("Data Source={0};Version=3;", databaseName));
+        }
+
+        //Executes a given query using the connection given
+        //CREATE TABLE oak_settings (name TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL) 
+        public static void ExecuteQuery(string sql, SQLiteConnection connection)
+        {
+            SQLiteCommand cmd = new SQLiteCommand(sql, connection);
+            cmd.ExecuteNonQuery();
+        }
+
+        //Creates initial tables for the database
+        //Initial tables:
+        /*
+        oak_settings
+        */
+        public static void CreateInitialTables(SQLiteConnection connection)
+        {
+            ExecuteQuery("CREATE TABLE oak_settings (name TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL)", connection);
+        }
+
+        //Writes a value to a setting based on the name (may be changed)
+        public static void WriteConfigValue(string settingName, string value, SQLiteConnection connection)
+        {
+            ExecuteQuery(string.Format("UPDATE oak_settings SET value = '{0}' WHERE name = '{1}'", value, settingName), connection);
         }
     }
 }
