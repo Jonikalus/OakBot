a9ac5e6ac80e3e62bb8098418ddeeee279671f4b
Jeanolos <iaf41nuerrenber@gso-koeln.de>
2016-01-14 19:25:51 +0100

    Removed database related stuff for now

---
+++ OakBot/Setting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OakBot
+{
+    class Setting
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public Setting(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
--- OakBot/Utils.cs
+++ OakBot/Utils.cs
@@ -11,6 +11,7 @@ namespace OakBot
 {
     class Utils
     {
+        public static List<Setting> settings = new List<Setting>();
         //Extracts the OAuth token from an URL
         public static string getAuthTokenFromUrl(string absoluteUrl)
         {
@@ -19,52 +20,5 @@ namespace OakBot
             return token;
         }
 
-        //Creates a database by file name
-        public static void CreateDatabase(string fileName)
-        {
-            if (!File.Exists(fileName))
-            {
-                SQLiteConnection.CreateFile(fileName);
-            }
-        }
-
-        //Connects to database by file name
-        public static SQLiteConnection ConnectDatabase(string databaseName)
-        {
-            return new SQLiteConnection(string.Format("Data Source={0};Version=3;", databaseName));
-        }
-
-        //Executes a given query using the connection given
-        //CREATE TABLE oak_settings (name TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL) 
-        public static void ExecuteQuery(string sql, SQLiteConnection connection)
-        {
-            SQLiteCommand cmd = new SQLiteCommand(sql, connection);
-            cmd.ExecuteNonQuery();
-        }
-
-        //Creates initial tables for the database
-        //Initial tables:
-        /*
-        oak_settings
-        */
-        public static void CreateInitialTables(SQLiteConnection connection)
-        {
-            ExecuteQuery("CREATE TABLE oak_settings (name TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL)", connection);
-        }
-
-        //Writes a value to a setting based on the name (may be changed)
-        public static void WriteConfigValue(string settingName, string value, SQLiteConnection connection)
-        {
-            ExecuteQuery(string.Format("UPDATE oak_settings SET value = '{0}' WHERE name = '{1}'", value, settingName), connection);
-        }
-
-        //Initial entries for the settings table
-        public static void CreateInitialSettings(SQLiteConnection connection)
-        {
-            ExecuteQuery("INSERT INTO oak_settings (name, value) VALUES ('streamerOAuthToken', 'please change')", connection);
-            ExecuteQuery("INSERT INTO oak_settings (name, value) VALUES ('botOAuthToken', 'please change')", connection);
-            ExecuteQuery("INSERT INTO oak_settings (name, value) VALUES ('streamerUserName', 'please change')", connection);
-            ExecuteQuery("INSERT INTO oak_settings (name, value) VALUES ('botUserName', 'please change')", connection);
-        }
     }
 }
