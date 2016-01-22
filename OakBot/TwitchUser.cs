31ab07d1a9dd00d04ec9dcd40288bdba801beefc
Ocgineer <rvanbeek1@gmail.com>
2016-01-22 14:19:59 +0100

    Small update on the available fields.

--- OakBot/TwitchUser.cs
+++ OakBot/TwitchUser.cs
@@ -56,14 +56,15 @@ namespace OakBot
         // isSub and isFollowing can also be set by the bot on event
 
         // Channel
-        public bool isFollowing { get; set; }
-        public bool isSub { get; set; }
+        public bool following { get; set; }     // Bool SQL
+        public bool subscriber { get; set; }    // Bool SQL
 
-        // Points, Hours and Rank
+        // Points, raids, Hours and Rankname
         public long points { get; set; }        // BigInt SQL
         public long raids { get; set; }         // BigInt SQL
-        public string rank { get; set; }
+        public string rank { get; set; }        // Text SQL
 
+        // Watched timespan
         public TimeSpan watchedTimeSpan { get; set; }
         public double hours
         {
@@ -72,36 +73,41 @@ namespace OakBot
                 return Math.Round(watchedTimeSpan.TotalHours, 1, MidpointRounding.AwayFromZero);
             }
         }
+        public long minutes // BigInt SQL
+        {
+            get
+            {
+                return Convert.ToInt64(watchedTimeSpan.TotalMinutes);
+            }
+        }
 
         // First seen and last message (seen)
         public DateTime dateLastSeen { get; set; }
         public DateTime dateFollow { get; set; }
 
         // Regular and indicator that streamer/mod removed regular
-        public bool isReg { get; set; }
+        public bool regular { get; set; }
         public bool forcedRegRemove { get; set; }
 
         // VIP Bronze
-        public bool isVIP1 { get; set; }
-        public string msgVIP1 { get; set; }
+        public bool VIP1 { get; set; }
         public DateTime gotVIP1 { get; set; }
         public DateTime expVIP1 { get; set; }
 
         // VIP Silver
-        public bool isVIP2 { get; set; }
-        public string msgVIP2 { get; set; }
+        public bool VIP2 { get; set; }
         public DateTime gotVIP2 { get; set; }
         public DateTime expVIP2 { get; set; }
 
         // VIP Gold
-        public bool isVIP3 { get; set; }
-        public string msgVIP3 { get; set; }
+        public bool VIP3 { get; set; }
         public DateTime gotVIP3 { get; set; }
         public DateTime expVIP3 { get; set; }
 
-        // JOIN and PART messages
+        // JOIN and PART messages and comment field
         public string msgJoin { get; set; }
         public string msgPart { get; set; }
+        public string comment { get; set; }
 
 
     }
