da6b5435f4ddb51c236e12ad2e287aa4acdf844d
Jeanolos <iaf41nuerrenber@gso-koeln.de>
2016-02-04 21:44:16 +0100

    Added a song class for songrequests and added the CefSharp library for a reliable browser

---
+++ OakBot/Song.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OakBot
+{
+    public class Song
+    {
+        public string SongName { get; set; }
+        public string Link { get; set; }
+        public SongType Type { get; }
+
+        public Song(string songname, string link)
+        {
+            SongName = songname;
+            Link = link;
+            if (Regex.IsMatch(Link, "soundcloud", RegexOptions.IgnoreCase))
+            {
+                Type = SongType.SOUNDCLOUD;
+            }else
+            {
+                Type = SongType.YOUTUBE;
+            }
+        }
+    }
+
+    public enum SongType
+    {
+        YOUTUBE,
+        SOUNDCLOUD
+    }
+}
